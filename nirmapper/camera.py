4379505bd8dc8bf0044cce8fe8b49c5f7d0c780d
Bernd Fecht <bernd.fecht1@hs-augsburg.de>
2018-12-13 11:15:51 +0100

    Moved functionality from mapper to camera class. Extended tests.

--- nirmapper/camera.py
+++ nirmapper/camera.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 from .utils import (
-    euler_angles_to_rotation_matrix
-)
+    euler_angles_to_rotation_matrix,
+    get_2d_coordinate_from_homogeneous_vector)
 
 
 class Camera(object):
@@ -24,27 +24,33 @@ class Camera(object):
         self.cam_location_xyz = cam_location_xyz
         self.cam_euler_rotation_theta = cam_euler_rotation_theta
 
+        self.A = np.array([])
+        self.D = np.array([])
+        self.P = np.array([])
+
     def get_intrinsic_3x4_A_matrix(self) -> np.ndarray:
         """
         Get intrinsic camera calibration matrix K.
 
         :return: Calibration Matrix K
         """
-        s_u = self.resolution_x / self.sensor_width_in_mm
-        s_v = self.resolution_y / self.sensor_height_in_mm
+        if self.A.size == 0:
+            s_u = self.resolution_x / self.sensor_width_in_mm
+            s_v = self.resolution_y / self.sensor_height_in_mm
 
-        alpha_u = self.focal_length_in_mm * s_u
-        alpha_v = self.focal_length_in_mm * s_v
-        u_0 = self.resolution_x / 2
-        v_0 = self.resolution_y / 2
-        skew = 0  # use only rectangular pixels
+            alpha_u = self.focal_length_in_mm * s_u
+            alpha_v = self.focal_length_in_mm * s_v
+            u_0 = self.resolution_x / 2
+            v_0 = self.resolution_y / 2
+            skew = 0  # use only rectangular pixels
 
-        A = np.array([[alpha_u, skew, u_0],
-                      [0, alpha_v, v_0],
+            A = np.array([[alpha_u, skew, u_0],
+                          [0, alpha_v, v_0],
                       [0, 0, 1]
                       ])
+            self.A = A
 
-        return A
+        return self.A
 
     def get_extrinsic_3x4_D_matrix(self) -> np.ndarray:
         """
@@ -52,20 +58,23 @@ class Camera(object):
 
         :return: Extrinsic matrix RT
         """
-        R_cam2cv = np.array([[1, 0, 0],
-                             [0, -1, 0],
-                             [0, 0, -1]
-                             ])
+        if self.D.size == 0:
+            R_cam2cv = np.array([[1, 0, 0],
+                                 [0, -1, 0],
+                                 [0, 0, -1]
+                                 ])
 
-        R_world2cam = euler_angles_to_rotation_matrix(self.cam_euler_rotation_theta).T
-        T_world2cam = np.dot(-1 * R_world2cam, self.cam_location_xyz)
+            R_world2cam = euler_angles_to_rotation_matrix(self.cam_euler_rotation_theta).T
+            T_world2cam = np.dot(-1 * R_world2cam, self.cam_location_xyz)
 
-        R_world2cv = np.dot(R_cam2cv, R_world2cam)
-        T_world2cv = R_cam2cv.dot(T_world2cam).reshape(3, 1)
+            R_world2cv = np.dot(R_cam2cv, R_world2cam)
+            T_world2cv = R_cam2cv.dot(T_world2cam).reshape(3, 1)
 
-        RT = np.hstack((R_world2cv, T_world2cv))
+            RT = np.hstack((R_world2cv, T_world2cv))
 
-        return RT
+            self.D = RT
+
+        return self.D
 
     def get_3x4_P_projection_matrix(self) -> np.ndarray:
         """
@@ -73,7 +82,59 @@ class Camera(object):
 
         :return: Combined matrix P
         """
-        A = self.get_intrinsic_3x4_A_matrix()
-        D = self.get_extrinsic_3x4_D_matrix()
+        if self.P.size == 0:
+            A = self.get_intrinsic_3x4_A_matrix()
+            D = self.get_extrinsic_3x4_D_matrix()
+            P = np.dot(A, D)
 
-        return np.dot(A, D)
+            self.P = P
+
+        return self.P
+
+    def project_world_point_to_pixel_coords(self, point: np.ndarray):
+        """
+        Calculates the image space coordinates for a coordinate in object space. The coordinate values will
+        be between the image pixel height and width.
+
+        :param numpy.array point: The three-dimensional coordinates in object space
+        :return numpy.array: Two-dimensional pixel coordinates in image space
+        """
+        P = self.get_3x4_P_projection_matrix()
+        # Append 3d coord with fourth param to calculate coordinate
+        uv_xyz = P.dot(np.append(point, 1))
+        uv_xy = get_2d_coordinate_from_homogeneous_vector(uv_xyz)
+
+        return uv_xy
+
+    def project_world_point_to_uv_coords(self, point: np.ndarray):
+        """
+        Calculates the image space UV coordinates for a coordinate in object space.
+        UV coordinates are between 0 and 1.
+
+        :param numpy.array point: The three-dimensional coordinates in object space
+        :return numpy.array: Two-dimensional uv coordinate in image space
+        """
+        uv_xy = self.project_world_point_to_pixel_coords(point)
+        uv_xy[0] = uv_xy[0] / self.resolution_x
+        uv_xy[1] = uv_xy[1] / self.resolution_y
+
+        return np.array(uv_xy)
+
+    def project_world_points_to_uv_coords(self, points: np.ndarray):
+        """
+        Calculates the image space UV coordinates for coordinates in object space.
+        UV coordinates are between 0 and 1.
+
+        :param numpy.array point: The three-dimensional coordinates in object space
+        :return numpy.array: Two-dimensional uv coordinates in image space
+        """
+        # User maybe just passed a single coord - fallback to other function
+        if points.ndim == 1:
+            return self.project_world_point_to_uv_coords(points)
+
+        uv_coords = []
+        for point in points:
+            uv_xy = self.project_world_point_to_uv_coords(point)
+            uv_coords.append(uv_xy)
+
+        return np.array(uv_coords)
--- nirmapper/test/test_camera.py
+++ nirmapper/test/test_camera.py
@@ -15,13 +15,19 @@ class TestCamera(TestCase):
         self.screen_width = 1920
         self.screen_height = 1080
 
-        self.cam = Camera(self.focal_length, self.screen_width, self.screen_height, self.sensor_width, self.sensor_height, self.location, self.rotation)
+        self.cam = Camera(self.focal_length, self.screen_width, self.screen_height, self.sensor_width,
+                          self.sensor_height, self.location, self.rotation)
+
+        self.p1 = np.array([1, 1, 1])
+        self.p2 = np.array([0, 1, 0])
+        self.p3 = np.array([0, 0, 1])
+        self.p4 = np.array([0, 0, 0])
 
     def test_get_intrinsic_3x4_A_matrix(self):
         intrinsic_mat = np.array([
-            [2100.0,    0.0,    960.0],
-            [0.0,       2100.0, 540.0],
-            [0.0,       0.0,    1.0]
+            [2100.0, 0.0, 960.0],
+            [0.0, 2100.0, 540.0],
+            [0.0, 0.0, 1.0]
         ])
 
         try:
@@ -56,6 +62,87 @@ class TestCamera(TestCase):
 
         try:
             np.testing.assert_almost_equal(self.cam.get_3x4_P_projection_matrix(), projection_mat)
+            res = True
+        except AssertionError as err:
+            res = False
+            print(err)
+        self.assertTrue(res)
+
+    def test_project_world_point_to_pixel_coords(self):
+        exp_p1 = np.array([1310, 890])
+        exp_p2 = np.array([960, 540])
+        exp_p3 = np.array([960, 840])
+        exp_p4 = np.array([960, 540])
+
+        pix_p1 = self.cam.project_world_point_to_pixel_coords(self.p1)
+        pix_p2 = self.cam.project_world_point_to_pixel_coords(self.p2)
+        pix_p3 = self.cam.project_world_point_to_pixel_coords(self.p3)
+        pix_p4 = self.cam.project_world_point_to_pixel_coords(self.p4)
+
+        try:
+            np.testing.assert_almost_equal(pix_p1, exp_p1)
+            np.testing.assert_almost_equal(pix_p2, exp_p2)
+            np.testing.assert_almost_equal(pix_p3, exp_p3)
+            np.testing.assert_almost_equal(pix_p4, exp_p4)
+            res = True
+        except AssertionError as err:
+            res = False
+            print(err)
+        self.assertTrue(res)
+
+    def test_project_world_points_to_uv_coords(self):
+        exp_p1 = np.array([0.7, 0.8])
+        exp_p2 = np.array([0.5, 0.5])
+        exp_p3 = np.array([0.5, 0.8])
+        exp_p4 = np.array([0.5, 0.5])
+
+        pix_p1 = (self.cam.project_world_point_to_uv_coords(self.p1)).round(decimals=1)
+        pix_p2 = (self.cam.project_world_point_to_uv_coords(self.p2)).round(decimals=1)
+        pix_p3 = (self.cam.project_world_point_to_uv_coords(self.p3)).round(decimals=1)
+        pix_p4 = (self.cam.project_world_point_to_uv_coords(self.p4)).round(decimals=1)
+
+        try:
+            np.testing.assert_equal(pix_p1, exp_p1)
+            np.testing.assert_equal(pix_p2, exp_p2)
+            np.testing.assert_equal(pix_p3, exp_p3)
+            np.testing.assert_equal(pix_p4, exp_p4)
+            res = True
+        except AssertionError as err:
+            res = False
+            print(err)
+        self.assertTrue(res)
+
+        points = np.array([
+            self.p1,
+            self.p2,
+            self.p3,
+            self.p4
+        ])
+
+        exp_points = np.array([
+            exp_p1,
+            exp_p2,
+            exp_p3,
+            exp_p4
+        ])
+
+        points = (self.cam.project_world_points_to_uv_coords(points)).round(decimals=1)
+
+        try:
+            np.testing.assert_equal(points, exp_points)
+            res = True
+        except AssertionError as err:
+            res = False
+            print(err)
+        self.assertTrue(res)
+
+    def test_project_world_points_to_uv_coords_fallback(self):
+        exp_p1 = np.array([0.7, 0.8])
+
+        pix_p1 = (self.cam.project_world_points_to_uv_coords(self.p1)).round(decimals=1)
+
+        try:
+            np.testing.assert_equal(pix_p1, exp_p1)
             res = True
         except AssertionError as err:
             res = False
