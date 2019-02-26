8f209286df9692cd43a5f30bf777a2e9ad9de2b5
Bernd Fecht <bernd.fecht1@hs-augsburg.de>
2019-02-26 12:28:27 +0100

    Changed some setup stuff.

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 setup(
     name='nirmapper',
     version='0.1.0',
-    packages=['nirmapper', 'nirmapper.model', 'nirmapper.renderer', 'nirmapper.test', 'nirmapper.resources',
+    packages=['nirmapper', 'nirmapper.model', 'nirmapper.renderer', 'nirmapper.tests', 'nirmapper.resources',
               'nirmapper.resources.images'],
     url='https://github.com/fechbmaster/3DNIRmapper',
     license='Apache-2.0',
     author='fechbmaster',
     author_email='bernd.fecht1@hs-augsburg.de',
     description='A 3D Mapper to map NIR images to a 3d tooth model.',
+    dependency_links=['https://github.com/pywavefront/PyWavefront', 'https://github.com/pycollada/pycollada'],
     install_requires=['numpy']
 )
