f4c612d0723baa979b7f36b85543f22512a9c800
Bernd Fecht <bernd.fecht1@hs-augsburg.de>
2019-02-27 15:15:14 +0100

    Extended setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nirmapper',
-    version='1.0.0',
+    version='1.1.1',
     packages=find_packages(exclude='tests'),
     url='https://github.com/fechbmaster/3DNIRmapper',
     license='Apache-2.0',
@@ -10,6 +10,7 @@ setup(
     author_email='bernd.fecht1@hs-augsburg.de',
     description='A 3D Mapper to map NIR images to a 3d tooth model.',
     dependency_links=['https://github.com/pywavefront/PyWavefront', 'https://github.com/pycollada/pycollada'],
-    install_requires=['numpy', 'Click'],
+    python_requires='>2.7',
+    install_requires=['numpy', 'Click', 'PyWavefront', 'pycollada'],
     scripts=['bin/nirmapper']
 )
